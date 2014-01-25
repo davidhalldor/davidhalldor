d17b567eb65ea9f718aca2ceabc0c1f94ec1acbb
Davíð Halldór Lúðvíksson <davidhalldor@gmail.com>
2014-01-25 15:07:09 +0000

    Nýtt app engine útgáfunúmer.

--- davidhaldlor/main.py
+++ davidhaldlor/main.py
@@ -20,6 +20,7 @@ import logging
 import json
 from google.appengine.ext import db
 
+
 class Person(db.Model):
     '''The data model'''
     name = db.StringProperty(required=True);
@@ -36,29 +37,35 @@ class Person(db.Model):
     sex = db.StringProperty(required=True);
     college = db.StringProperty(required=True);
     university = db.StringProperty(required=True);
-    
+
     '''Returning db object to dict obj'''
+
     def to_dict(self):
-        return db.to_dict(self, {'id':self.key().id()})
+        return db.to_dict(self, {'id': self.key().id()})
+
 
 class Employee(db.Model):
     '''The data model'''
-    employee = db.StringProperty(required=True);
-    
+    name = db.StringProperty(required=True);
+
     person = db.ReferenceProperty(Person,
-                                   collection_name='employees')
+                                  collection_name='employees')
 
     '''Returning db object to dict obj'''
+
     def to_dict(self):
-        return db.to_dict(self, {'id':self.key().id()})
+        return db.to_dict(self, {'id': self.key().id()})
+
 
 class Skill(db.Model):
     '''The data model'''
     description = db.StringProperty(required=True);
-    
+
     '''Returning db object to dict obj'''
+
     def to_dict(self):
-        return db.to_dict(self, {'id':self.key().id()})
+        return db.to_dict(self, {'id': self.key().id()})
+
 
 class PersonAPI(webapp2.RequestHandler):
     def get(self):
@@ -71,50 +78,43 @@ class PersonAPI(webapp2.RequestHandler):
 
     def post(self):
         dictonary = json.loads(self.request.body)
-        person = Person(name = dictonary['name'],
-                            address = dictonary['address'],
-                            telePhone = dictonary['telePhone'],
-                            cellPhone = dictonary['cellPhone'],
-                            email = dictonary['email'],
-                            dateOfBirth = dictonary['dateOfBirth'],
-                            placeOfBirth = dictonary['placeOfBirth'],
-                            citizenship = dictonary['citizenship'],
-                            maritalStatus = dictonary['maritalStatus'],
-                            spoucesName = dictonary['spoucesName'],
-                            children = dictonary['children'],
-                            sex = dictonary['sex'],
-                            college = dictonary['college'],
-                            university = dictonary['university'])
+        person = Person(name=dictonary['name'],
+                        address=dictonary['address'],
+                        telePhone=dictonary['telePhone'],
+                        cellPhone=dictonary['cellPhone'],
+                        email=dictonary['email'],
+                        dateOfBirth=dictonary['dateOfBirth'],
+                        placeOfBirth=dictonary['placeOfBirth'],
+                        citizenship=dictonary['citizenship'],
+                        maritalStatus=dictonary['maritalStatus'],
+                        spoucesName=dictonary['spoucesName'],
+                        children=dictonary['children'],
+                        sex=dictonary['sex'],
+                        college=dictonary['college'],
+                        university=dictonary['university'])
         person.put()
+
 
 class EmployeeAPI(webapp2.RequestHandler):
     def get(self):
         '''Fetching 1 records from db model'''
-        person = Person.all().fetch(limit=1)
-        logging.info([p.to_dict() for p in person])
+        employee = Employee.all().fetch(limit=1)
+        logging.info([e.to_dict() for e in employee])
         '''Returning the db records as list of dictonaries for ez json conversion'''
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps([p.to_dict() for p in person]))
+        self.response.out.write(json.dumps([e.to_dict() for e in employee]))
 
     def post(self):
         dictonary = json.loads(self.request.body)
-        person = Person(name = dictonary['name'],
-                            address = dictonary['address'],
-                            telePhone = dictonary['telePhone'],
-                            cellPhone = dictonary['cellPhone'],
-                            email = dictonary['email'],
-                            dateOfBirth = dictonary['dateOfBirth'],
-                            placeOfBirth = dictonary['placeOfBirth'],
-                            citizenship = dictonary['citizenship'],
-                            maritalStatus = dictonary['maritalStatus'],
-                            spoucesName = dictonary['spoucesName'],
-                            children = dictonary['children'],
-                            sex = dictonary['sex'],
-                            college = dictonary['college'],
-                            university = dictonary['university'])
-        person.put()
+        employee = Employee(name=dictonary['name'])
+        q = Person.all()
+        q.filter('name =', dictonary['person'])
+        person = q.get()
+        employee.person = person
+        employee.put()
+
 
 app = webapp2.WSGIApplication([
-    ('/api/person.*', PersonAPI),
-    ('/api/employee.*', EmployeeAPI)
-], debug=True)
+                                  ('/api/person.*', PersonAPI),
+                                  ('/api/employee.*', EmployeeAPI)
+                              ], debug=True)
--- davidhaldlor/scripts/controller.js
+++ davidhaldlor/scripts/controller.js
@@ -1,5 +1,11 @@
-function DataListCtrl($scope, $http) {
+function PersonListCtrl($scope, $http) {
   $http.get('/api/person').success(function(data) {
+    $scope.items = data;
+  });
+}
+
+function EmployeeListCtrl($scope, $http) {
+  $http.get('/api/employee').success(function(data) {
     $scope.items = data;
   });
 }
